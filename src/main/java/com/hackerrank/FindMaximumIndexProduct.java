f3d6e4c9a886bb98538021a57cc18e18797c56f0
taka wh7 <takawh7@gmail.com>
2018-03-08 20:52:54 +0900

    refactor fmip

--- src/main/java/com/hackerrank/FindMaximumIndexProduct.java
+++ src/main/java/com/hackerrank/FindMaximumIndexProduct.java
@@ -1,9 +1,10 @@
 package com.hackerrank;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.IntStream;
 
 public class FindMaximumIndexProduct {
     public static void main(String[] args) {
@@ -13,37 +14,37 @@ public class FindMaximumIndexProduct {
 //            long[] numbers = IntStream.rangeClosed(1, n)
 //                    .mapToLong(j -> Long.parseLong(sc.next()))
 //                    .toArray();
-            File file = new File("../JavaProgrammingTraining/src/main/java/com/hackerrank/text/fmip.txt");
+            File file = new File("../JavaProgrammingTraining/src/main/java/com/hackerrank/text/fmip3.txt");
             BufferedReader br = new BufferedReader(new FileReader(file));
             int n = Integer.parseInt(br.readLine());
             String[] lines = br.readLine().split(" ");
-            System.out.println(lines.length);
             long[] numbers = Arrays.stream(lines).mapToLong(s -> Long.parseLong(s)).toArray();
             long[] products = new long[n];
-//            products[0] = 0;
-            products[n - 1] = 0;
 
-            IntStream.range(1, n).forEach(i -> {
-                int l = i;
+            long start = System.currentTimeMillis();
+
+            for (int i = 1; i < n; i++) {
                 // search for left
-                long left = IntStream.range(0, l)
-                        .boxed()
-                        .sorted(Comparator.reverseOrder())
-                        .filter(j -> numbers[l] < numbers[j])
-                        .mapToLong(j -> j + 1)
-                        .findFirst()
-                        .orElse(0);
-
+                long left = 0;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (numbers[i] < numbers[j]) {
+                        left = j + 1;
+                        break;
+                    }
+                }
                 // search for right
-                long right = IntStream.range(l, n)
-                        .filter(k -> numbers[l] < numbers[k])
-                        .mapToLong(k -> k + 1)
-                        .findFirst()
-                        .orElse(0);
+                long right = 0;
+                for (int k = i; k < n; k++) {
+                    if (numbers[i] < numbers[k]) {
+                        right = k + 1;
+                        break;
+                    }
+                }
+                products[i] = left * right;
+            }
 
-                products[l] = left * right;
-            });
-
+            long end = System.currentTimeMillis();
+            System.out.println((end - start) + "ms");
             System.out.println(Arrays.stream(products).max().orElse(0));
         } catch (IOException e) {
             e.printStackTrace();
